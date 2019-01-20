ba412d18093d6d49df94344b6af8ad8ddcb9507a
BeigeHacker <BeigeHacker@users.noreply.github.com>
2019-01-20 21:30:11 +0000

    Added Grid layout to UI Added Grid to main app for body of feature pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header/Header';
+import Grid from './Grid/Grid';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
       <Header />
+      <Grid />
       </div>
     );
   }
